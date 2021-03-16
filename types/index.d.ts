54a8cb402044ed734da68b9e16299d5b3fa3767f
yuki0410_ <>
2021-03-16 23:53:38 +0900

    fix type

--- types/index.d.ts
+++ types/index.d.ts
@@ -31,7 +31,7 @@ export class Modal {
  * Modal Options
  */
 export interface ModalOptions {
-  focusTrap?: Pick<FocusTrapOptions, "escapeDeactivates" | "clickOutsideDeactivates">;
+  focusTrap?: Omit<FocusTrapOptions, "escapeDeactivates" | "clickOutsideDeactivates">;
   bodyScrollLock?: patchedBodyScrollOptions;
 }
 
